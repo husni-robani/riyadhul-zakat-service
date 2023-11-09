a44427434c0046f40d447033b2d9781fa354d487
husni-robani <husnir2005@gmail.com>
2023-11-09 10:47:55 +0700

    create new function for add new transaction

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTransactionRequest;
+use App\Http\Resources\TransactionResource;
+use App\Models\Donor;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Nette\Schema\ValidationException;
 
 class TransactionController extends Controller
 {
-    //
+    public function store(StoreTransactionRequest $request){
+        try{
+            $donor = Donor::findOrFail($request->get("donors_id"));
+            $transaction = $donor->transactions()->create([
+                "donors_id" => $donor->get("id"),
+                "donation_type" => $request->get('donation_type'),
+                "payment_method" => $request->get('payment_method'),
+                "total_money" => $request->get('total_money'),
+                "total_good" => $request->get('total_good'),
+                "status" => false
+            ]);
+            return response()->json([
+                "message" => "create new transaction successful",
+                "data" => new TransactionResource($transaction)
+            ]);
+        }catch (ModelNotFoundException | \Exception | ValidationException $exception){
+            $statusCode = $exception instanceof ModelNotFoundException ? 404 : ($exception instanceof ValidationException ? 422 : 400);
+
+            return response()->json([
+                "errors" => $exception->getMessage()
+            ], $statusCode);
+        }
+    }
 }
