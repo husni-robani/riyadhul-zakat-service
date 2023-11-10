7e7080ba2956a4ecb7b03c4f0c835ddbe57910af
husni-robani <husnir2005@gmail.com>
2023-11-10 13:20:07 +0700

    add new attribute is_admin to seeding user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'test',
             'email' => 'test@example.com',
             'password' => 'password',
+            'is_admin' => true
         ]);
 
         $husniDonation = Donor::create([
